f62f88e2dccab7e1f7d1ce12c1153f36f6ae6fe2
anazhmetdin <anazhmetdin@gmail.com>
2022-07-14 14:12:52 +0200

    adjust the direction ring after resize

--- js/app.js
+++ js/app.js
@@ -1,24 +1,25 @@
 let POINTER_MOVING = false;
-let directionAngle, ringCenterX, ringCenterY, pointer0X, pointer0Y;
+let directionAngle, ringCenterX, ringCenterY;
 let directionElement, pointerElement;
+
+function setDirectionRingCordinates() {
+    const DIRECTION_RECT = directionElement.getBoundingClientRect();  
+    ringCenterX = (DIRECTION_RECT.left + DIRECTION_RECT.right) / 2;
+    ringCenterY = DIRECTION_RECT.y + (DIRECTION_RECT.height / 2);
+}
 
 // once the document is loaded
 document.addEventListener('DOMContentLoaded', () => {
 
-    directionElement = document.querySelector('#direction')
-    
-    const DIRECTION_RECT = directionElement.getBoundingClientRect();  
-    
-    ringCenterX = (DIRECTION_RECT.left + DIRECTION_RECT.right) / 2;
-    ringCenterY = DIRECTION_RECT.y + (DIRECTION_RECT.height / 2);
-
+    //#####################################################
+    // set-up direction ring
+    //#####################################################
+    // get ring elments
+    directionElement = document.querySelector('#direction');
     pointerElement = directionElement.lastElementChild;
-
-    const POINTER_RECT = pointerElement.getBoundingClientRect();
-
-    pointer0X = (POINTER_RECT.left + POINTER_RECT.right) / 2;
-    pointer0Y = POINTER_RECT.y + (POINTER_RECT.height / 2);
-
+    // get cordinates
+    setDirectionRingCordinates();
+    // listen to dragging the pointer
     pointerElement.addEventListener('mousedown', () => POINTER_MOVING = true);
 
     document.addEventListener('mouseup', () => {
@@ -34,5 +35,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    console.log(directionAngle);
+    // listen to resizing the window, update ring cordinates
+    window.addEventListener('resize', setDirectionRingCordinates);
 });
