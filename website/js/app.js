d9c2ad3c0dc908e3069bc4d1421dc77f08464d18
anazhmetdin <anazhmetdin@gmail.com>
2022-07-15 12:47:23 +0200

    allow cross-origin and control audio

--- website/js/app.js
+++ website/js/app.js
@@ -1,8 +1,8 @@
 let pointerIsMoving = false;
 let directionAngle, ringCenterX, ringCenterY;
 let directionElement, pointerElement, directionRect;
-const AMBIENT_RATIO = 0.1
-let audioPlayer, audioContext, panNode;
+const AMBIENT_RATIO = 0.9
+let audioPlayer, audioContext = null, panNode, gainNode;
 
 function setDirectionRingCordinates() {
     ringCenterX = (directionRect.left + directionRect.right) / 2;
@@ -11,30 +11,38 @@ function setDirectionRingCordinates() {
 
 function setAudioRatio() {
     const RAD = directionAngle * Math.PI / 180
-    const SIN = Math.sin(RAD) + AMBIENT_RATIO;
-    const COS = Math.abs(Math.cos(RAD))  + AMBIENT_RATIO;
-    const BACK_DIRECTION_REDUCTION = 1 - 0.25 * (COS - AMBIENT_RATIO);
-    let leftRatio, rightRatio;
+    const BALANCE = Math.sin(RAD) * AMBIENT_RATIO;
+    const COS = Math.abs(Math.cos(RAD));
+    const BACK_DIRECTION_REDUCTION = 1 - 0.35 * COS;
+    let gain = 1;
+    // let leftRatio, rightRatio;
     
-    if (directionAngle <= 180) {
-        leftRatio = COS;
-        rightRatio = 1 + SIN;
-    } else {
-        leftRatio = 1 - SIN + 2*AMBIENT_RATIO;
-        rightRatio = COS;
-    }
+    // if (directionAngle <= 180) {
+    //     leftRatio = COS;
+    //     rightRatio = 1 + blanace;
+    // } else {
+    //     leftRatio = 1 - blanace + 2*AMBIENT_RATIO;
+    //     rightRatio = COS;
+    // }
 
     if (directionAngle > 90 && directionAngle < 270) {
-        leftRatio *= BACK_DIRECTION_REDUCTION;
-        rightRatio *= BACK_DIRECTION_REDUCTION;
+        // leftRatio *= BACK_DIRECTION_REDUCTION;
+        // rightRatio *= BACK_DIRECTION_REDUCTION;
+        gain = BACK_DIRECTION_REDUCTION;
     }
 
-    return leftRatio/rightRatio - 1
+    return {balance: BALANCE, gain: gain};
 }
 
 function updateDirection(e) {
     directionAngle = 180 - (Math.atan2(e.clientX - ringCenterX, e.clientY - ringCenterY) * (180/Math.PI));
     directionElement.style.transform = `rotate(${directionAngle}deg)`
+}
+
+function modifyAudio() {
+    let audio = setAudioRatio();
+    panNode.pan.value = audio.balance;
+    gainNode.gain.value = audio.gain;
 }
 
 // once the document is loaded
@@ -49,11 +57,18 @@ document.addEventListener('DOMContentLoaded', () => {
     pointerElement = directionElement.lastElementChild;
     
     audioPlayer = document.querySelector('#player');
-    audioContext = new window.AudioContext();
-    const AUDIO_SOURCE = audioContext.createMediaElementSource(audioPlayer);
-    panNode = audioContext.createStereoPanner();
-    AUDIO_SOURCE.connect(panNode);
-    panNode.connect(audioContext.destination);
+    
+    audioPlayer.addEventListener('play', () => {
+        if (audioContext === null) {
+            audioContext = new window.AudioContext();
+            const AUDIO_SOURCE = audioContext.createMediaElementSource(audioPlayer);
+            panNode = new StereoPannerNode(audioContext, { pan: 0});
+            gainNode = new GainNode(audioContext, {gain: 1})
+            AUDIO_SOURCE.connect(panNode);
+            panNode.connect(gainNode);
+            gainNode.connect(audioContext.destination);
+        }
+    })
 
     // get cordinates
     setDirectionRingCordinates();
@@ -64,6 +79,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const POINTER_MOVER = (e) => {
         pointerIsMoving = true;
         updateDirection(e);
+        modifyAudio();
     };
     directionElement.querySelector('#direction_ring').addEventListener('mousedown', () => POINTER_MOVER);
     directionElement.addEventListener('mousedown', POINTER_MOVER);
@@ -76,9 +92,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.addEventListener('mousemove', (e) => {
         if (pointerIsMoving) {
-            updateDirection(e);            
-            panNode.pan.value = setAudioRatio();
-            console.log(panNode.pan.value)
+            updateDirection(e);
+            modifyAudio();
         }
     });
 
