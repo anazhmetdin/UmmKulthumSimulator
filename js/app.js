d489e4521379d5dd52622ffe03dd41cbf21c1ee2
anazhmetdin <anazhmetdin@gmail.com>
2022-07-15 02:02:16 +0200

    change direction when ring is clicked

--- js/app.js
+++ js/app.js
@@ -1,33 +1,37 @@
 let POINTER_MOVING = false;
 let directionAngle, ringCenterX, ringCenterY;
 let leftRatio, rightRatio;
-let directionElement, pointerElement;
+let directionElement, pointerElement, directionRect;
+const AMBIENT_RATIO = 0.1
 
 function setDirectionRingCordinates() {
-    const DIRECTION_RECT = directionElement.getBoundingClientRect();  
-    ringCenterX = (DIRECTION_RECT.left + DIRECTION_RECT.right) / 2;
-    ringCenterY = DIRECTION_RECT.y + (DIRECTION_RECT.height / 2);
+    ringCenterX = (directionRect.left + directionRect.right) / 2;
+    ringCenterY = directionRect.y + (directionRect.height / 2);
 }
 
 function setAudioRatio() {
-    const AMBIENT = 0.1
     const RAD = directionAngle * Math.PI / 180
-    const SIN = Math.sin(RAD) + AMBIENT;
-    const COS = Math.abs(Math.cos(RAD))  + AMBIENT;
+    const SIN = Math.sin(RAD) + AMBIENT_RATIO;
+    const COS = Math.abs(Math.cos(RAD))  + AMBIENT_RATIO;
+    const BACK_DIRECTION_REDUCTION = 1 - 0.25 * (COS - AMBIENT_RATIO);
     
     if (directionAngle <= 180) {
         leftRatio = COS;
         rightRatio = 1 + SIN;
     } else {
-        leftRatio = 1 - SIN + 2*AMBIENT;
+        leftRatio = 1 - SIN + 2*AMBIENT_RATIO;
         rightRatio = COS;
     }
 
     if (directionAngle > 90 && directionAngle < 270) {
-        const BACK_DIRECTION_REDUCTION = 1 - 0.25 * Math.sin(RAD - Math.PI/2);
         leftRatio *= BACK_DIRECTION_REDUCTION;
         rightRatio *= BACK_DIRECTION_REDUCTION;
     }
+}
+
+function updateDirection(e) {
+    directionAngle = 180 - (Math.atan2(e.clientX - ringCenterX, e.clientY - ringCenterY) * (180/Math.PI));
+    directionElement.style.transform = `rotate(${directionAngle}deg)`
 }
 
 // once the document is loaded
@@ -38,13 +42,19 @@ document.addEventListener('DOMContentLoaded', () => {
     //#####################################################
     // get ring elments
     directionElement = document.querySelector('#direction');
+    directionRect = directionElement.getBoundingClientRect();
     pointerElement = directionElement.lastElementChild;
     // get cordinates
     setDirectionRingCordinates();
     // initialize sterio ratios
     setAudioRatio();
     // listen to dragging the pointer
-    directionElement.querySelector('#direction_ring').addEventListener('mousedown', () => POINTER_MOVING = true);
+    const POINTER_MOVER = (e) => {
+        POINTER_MOVING = true;
+        updateDirection(e);
+    };
+    directionElement.querySelector('#direction_ring').addEventListener('mousedown', () => POINTER_MOVER);
+    directionElement.addEventListener('mousedown', POINTER_MOVER);
 
     document.addEventListener('mouseup', () => {
         if (POINTER_MOVING) {
@@ -54,8 +64,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.addEventListener('mousemove', (e) => {
         if (POINTER_MOVING) {
-            directionAngle = 180 - (Math.atan2(e.clientX - ringCenterX, e.clientY - ringCenterY) * (180/Math.PI));
-            directionElement.style.transform = `rotate(${directionAngle}deg)`
+            updateDirection(e);
             setAudioRatio();
         }
     });
