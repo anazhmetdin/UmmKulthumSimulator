58f60c5a0323940f0a223d875c9fa62871ed1098
anazhmetdin <anazhmetdin@gmail.com>
2024-09-08 20:09:30 +0300

    init angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'umm-kulthum-simulator';
+  audioPath = "assets/ تهملني - فيروز.mp3";
+}
